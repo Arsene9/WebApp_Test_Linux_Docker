efee33e7daefa5629ad5ac3cb5a6c7fa78c0e730
Arsene9 <arsene.foka@yahoo.fr>
2023-06-15 16:35:43 -0500

    Add project files.

---
+++ WebApp_Test_Linux_Docker/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApp_Test_Linux_Docker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
